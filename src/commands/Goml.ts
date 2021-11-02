4a92dceaa747ce1eb1376b39259ac91791bcfd84
Cameron Smith <cjonsmith@gmail.com>
2021-11-01 22:57:38 -0700

    Use channel ID when referring to a channel.
    Discord JS channel fetching functionality works off of channel ID and
    not channel name, so we were not properly comparing against a channel
    name, but rather the channel ID.

--- src/commands/Goml.ts
+++ src/commands/Goml.ts
@@ -2,7 +2,7 @@ import { DMChannel, NewsChannel, TextChannel } from "discord.js";
 import DiscordClient, { MESSAGES } from "../DiscordClient";
 
 async function goml(client: DiscordClient, channel: DMChannel | TextChannel | NewsChannel): Promise<void> {
-  if (channel instanceof DMChannel || channel.name !== DiscordClient.CHANNELS.MAIN) return;
+  if (channel instanceof DMChannel || channel.id !== DiscordClient.CHANNEL_IDS.MAIN) return;
 
   await client.sendMessage(MESSAGES.GOML, DiscordClient.CHANNEL_IDS.MAIN);
 }
