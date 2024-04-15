505322841d5ee31e92d25a119a144f175ac67ac4
Keevan Dance <keevandance@gmail.com>
2024-04-15 10:19:05 +0900

    Add try catches to all message to get some insights where app is failing

--- src/NYTCrosswordPuzzleBot.ts
+++ src/NYTCrosswordPuzzleBot.ts
@@ -4,27 +4,52 @@ import DiscordClient, { MESSAGES } from "./DiscordClient";
 import Cron, { CRON_INTERVALS } from "./Cron";
 import goml from "./commands/Goml";
 import { Message } from "discord.js";
+import { getErrorMessage } from "./utils";
 
 const discordClient = new DiscordClient();
 
 discordClient.login(process.env.TOKEN || "");
 
 discordClient.on("ready", async () => {
-  await discordClient.sendMessage(MESSAGES.UPDATE, DiscordClient.CHANNEL_IDS.CHANGELOG);
+  try {
+    await discordClient.sendMessage(MESSAGES.UPDATE, DiscordClient.CHANNEL_IDS.CHANGELOG);
+  } catch (e: unknown) {
+    const error = getErrorMessage(e);
+    console.log("log: Cannot send update message");
+    console.log("error: ", error);
+  }
 
   //NYT Crossword updates at 10PM EST on weekdays and 6PM EST on weekends
   Cron.startJob(CRON_INTERVALS.WEEKDAY, async () => {
-    await discordClient.sendMessage(MESSAGES.PUZZLE, DiscordClient.CHANNEL_IDS.MAIN);
+    try {
+      await discordClient.sendMessage(MESSAGES.PUZZLE, DiscordClient.CHANNEL_IDS.MAIN);
+    } catch (e: unknown) {
+      const error = getErrorMessage(e);
+      console.log("log: Cannot send weekday puzzle message");
+      console.log("error: ", error);
+    }
   });
 
   Cron.startJob(CRON_INTERVALS.WEEKEND, async () => {
-    await discordClient.sendMessage(MESSAGES.PUZZLE, DiscordClient.CHANNEL_IDS.MAIN);
+    try {
+      await discordClient.sendMessage(MESSAGES.PUZZLE, DiscordClient.CHANNEL_IDS.MAIN);
+    } catch (e: unknown) {
+      const error = getErrorMessage(e);
+      console.log("log: Cannot send weekend puzzle message");
+      console.log("error: ", error);
+    }
   });
 });
 
 discordClient.on("messageCreate", async (message: Message) => {
   const parsedMessage = discordClient.parseMessage(message.content);
   if (parsedMessage === "goml" && message.channel.isText()) {
-    await goml(discordClient, message.channel);
+    try {
+      await goml(discordClient, message.channel);
+    } catch (e: unknown) {
+      const error = getErrorMessage(e);
+      console.log("log: Cannot send goml message");
+      console.log("error: ", error);
+    }
   }
 });
